82f34f028c02741fa638a962e80ef816a34c760b
Kaminho <kaminho16@gmail.com>
2017-05-29 17:09:03 +0200

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# FitShop project
+
+Aplikacja umożliwia tworzenie planu żywieniowego przez dietetyka lub trenera personalnego. Pozwala na sprawdzanie kaloryczności produktów i posiłków. Użytkownik może kupić produkty w sklepie online. 
+
+# Podstawowe funkcjonalności
+
+- Rejestracja, logowanie,
+- dodanie i edycja diety,
+- zakup produktów online.
---
+++ fitshop/fitshop/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace fitshop.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        public AccountController()
+        {
+           
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            //IdentityResult result = await _repo.RegisterUser(userModel);
+
+            //IHttpActionResult errorResult = GetErrorResult();
+
+            //if (errorResult != null)
+            //    return errorResult;
+
+            return Ok();
+        }
+        
+    }
+}
