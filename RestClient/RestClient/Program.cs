43834ee056d7346d3fb74bb63d602eb8868d132a
Janusz Miczuga <zyzzxeosnar@gmail.com>
2017-06-02 22:07:31 +0200

    Add files via upload

---
+++ RestClient/RestClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace RestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55913/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //chyba tutaj pobrane coś zostaje i mam nadzieję, że działczy :D
+            }
+        }
+    }
+}
